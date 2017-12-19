09dc721eb8bc2377f256b6a80826cd18590ff036
Jeff Niu <jeffniu22@gmail.com>
2017-12-19 12:23:46 -0800

    Updated twitter_columns.json

--- src/main/java/com/yahoo/bullet/twitter/Main.java
+++ src/main/java/com/yahoo/bullet/twitter/Main.java
@@ -1,8 +1,20 @@
 package com.yahoo.bullet.twitter;
 
+import com.yahoo.bullet.twitter.model.ColumnWriter;
+import com.yahoo.bullet.twitter.model.TwitterPlace;
+import com.yahoo.bullet.twitter.model.TwitterStatus;
+import com.yahoo.bullet.twitter.model.TwitterUser;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws IOException {
+        ColumnWriter writer = new ColumnWriter();
+        writer.writeColumnsFor(TwitterStatus.class, null);
+        writer.writeColumnsFor(TwitterPlace.class, "place");
+        writer.writeColumnsFor(TwitterUser.class, "user");
+        writer.writeColumnsTo(System.out, true);
     }
 
 }
